45759787feb35018244c812afaa65ef57804638d
Petri Laarne <petri.laarne@helsinki.fi>
2020-03-06 12:46:30 +0200

    Use a 3D grid in conditional MI estimation

--- ennemi/entropy_estimators.py
+++ ennemi/entropy_estimators.py
@@ -37,7 +37,9 @@ def estimate_single_mi(x : np.ndarray, y : np.ndarray, k : int = 3):
 
     # We use the fastest O(N*sqrt(k)) time algorithm, based on boxes
     # Create the 2D grid for finding the k-th neighbor
-    grid = _BoxGrid2D(x, y, k)
+    # TODO Performance: Having specialized 2D and 3D grids would benefit this
+    # method by ~10%, but at the expense of duplicated code.
+    grid = _BoxGrid(x, y, k)
 
     # Sort the observation arrays
     # These arrays is used for finding the axis neighbors by binary search
@@ -52,7 +54,7 @@ def estimate_single_mi(x : np.ndarray, y : np.ndarray, k : int = 3):
         cur_x = x[i]
         cur_y = y[i]
 
-        eps = _find_kth_neighbor(k, grid, cur_x, cur_y)
+        eps = _find_kth_neighbor(k, grid, cur_x, cur_y, 0)
 
         # Now eps contains the distance to the k'th neighbor
         # Find the number of x and y neighbors within that distance
@@ -84,18 +86,18 @@ def estimate_conditional_mi(x : np.ndarray, y : np.ndarray, cond : np.ndarray,
 
     # This is a straightforward extension of estimate_single_mi,
     # except that we cannot use _count_within for 2D projections.
-    inds = np.lexsort((cond, y, x))
+    grid = _BoxGrid(x, y, k, cond)
     zs = np.sort(cond)
 
     nxz = np.empty(N, np.int32)
     nyz = np.empty(N, np.int32)
     nz = np.empty(N, np.int32)
     for i in range(0, N):
-        cur_x = x[inds[i]]
-        cur_y = y[inds[i]]
-        cur_z = cond[inds[i]]
+        cur_x = x[i]
+        cur_y = y[i]
+        cur_z = cond[i]
 
-        eps = _find_kth_neighbor_slow(k, inds, i, x, y, cond)
+        eps = _find_kth_neighbor(k, grid, cur_x, cur_y, cur_z)
 
         # We can use _count_within only in one case
         nz[i] = _count_within(zs, cur_z - eps, cur_z + eps)
@@ -110,7 +112,7 @@ def estimate_conditional_mi(x : np.ndarray, y : np.ndarray, cond : np.ndarray,
     return psi(k) - np.mean(psi(nxz) + psi(nyz) - psi(nz))
 
 
-def _find_kth_neighbor(k, grid, cur_x, cur_y):
+def _find_kth_neighbor(k, grid, cur_x, cur_y, cur_z):
     # Start with the box containing the point. Then until we have found
     # enough neighbors, extend the rectangle to the most potential direction.
     # We should not go like peeling an onion because the boxes are not
@@ -138,22 +140,26 @@ def _find_kth_neighbor(k, grid, cur_x, cur_y):
     distances = np.full(k, eps)
 
     # First go through the points in the current box
-    box_x, box_y = grid.find_box(cur_x, cur_y)
-    eps = _update_epsilon(distances, eps, cur_x, cur_y, grid.boxes[box_x, box_y])
+    box_x, box_y, box_z = grid.find_box(cur_x, cur_y, cur_z)
+    eps = _update_epsilon(distances, eps, cur_x, cur_y, cur_z, grid.boxes[box_x, box_y, box_z])
 
     # Then extend to the most promising direction as long as necessary    
     left = right = box_x
     down = up = box_y
-    M = grid.boxes_per_axis - 1
+    back = front = box_z
+    M = grid.xy_boxes - 1
+    Z = grid.z_boxes - 1
 
     while not (left == 0 and right == M and down == 0 and up == M):
 
         # Find the direction where the edge is the closest
-        edge_dist = np.full(4, np.inf)
+        edge_dist = np.full(6, np.inf)
         if left > 0: edge_dist[0] = cur_x - grid.x_splits[left]
         if right < M: edge_dist[1] = grid.x_splits[right+1] - cur_x
         if down > 0: edge_dist[2] = cur_y - grid.y_splits[down]
         if up < M: edge_dist[3] = grid.y_splits[up+1] - cur_y
+        if back > 0: edge_dist[4] = cur_z - grid.z_splits[back]
+        if front < Z: edge_dist[5] = grid.z_splits[front+1] - cur_z
 
         direction = np.argmin(edge_dist)
 
@@ -168,80 +174,51 @@ def _find_kth_neighbor(k, grid, cur_x, cur_y):
             # Go left
             left -= 1
             for j in range(down, up+1):
-                eps = _update_epsilon(distances, eps, cur_x, cur_y, grid.boxes[left, j])
+                for k in range(back, front+1):
+                    eps = _update_epsilon(distances, eps, cur_x, cur_y, cur_z, grid.boxes[left, j, k])
         elif direction == 1:
             # Go right
             right += 1
             for j in range(down, up+1):
-                eps = _update_epsilon(distances, eps, cur_x, cur_y, grid.boxes[right, j])
+                for k in range(back, front+1):
+                    eps = _update_epsilon(distances, eps, cur_x, cur_y, cur_z, grid.boxes[right, j, k])
         elif direction == 2:
             # Go down
             down -= 1
             for i in range(left, right+1):
-                eps = _update_epsilon(distances, eps, cur_x, cur_y, grid.boxes[i, down])
+                for k in range(back, front+1):
+                    eps = _update_epsilon(distances, eps, cur_x, cur_y, cur_z, grid.boxes[i, down, k])
         elif direction == 3:
             # Go up
             up += 1
             for i in range(left, right+1):
-                eps = _update_epsilon(distances, eps, cur_x, cur_y, grid.boxes[i, up])
+                for k in range(back, front+1):
+                    eps = _update_epsilon(distances, eps, cur_x, cur_y, cur_z, grid.boxes[i, up, k])
+        elif direction == 4:
+            # Go back
+            back -= 1
+            for i in range(left, right+1):
+                for j in range(down, up+1):
+                    eps = _update_epsilon(distances, eps, cur_x, cur_y, cur_z, grid.boxes[i, j, back])
+        elif direction == 5:
+            # Go forward
+            front += 1
+            for i in range(left, right+1):
+                for j in range(down, up+1):
+                    eps = _update_epsilon(distances, eps, cur_x, cur_y, cur_z, grid.boxes[i, j, front])
 
     # This is reachable if the space contains just one box
     return eps
 
 
-def _update_epsilon(distances, eps, cur_x, cur_y, box):
-    for (x, y) in box:
-        dist = max(abs(cur_x - x), abs(cur_y - y))
+def _update_epsilon(distances, eps, cur_x, cur_y, cur_z, box):
+    for (x, y, z) in box:
+        dist = max(abs(cur_x - x), abs(cur_y - y), abs(cur_z - z))
 
         # Do not count the point itself
         if 0 < dist < eps:
             # Replace the largest distance in the array, then re-sort
             distances[len(distances)-1] = dist
-            distances.sort()
-            eps = distances.max()
-    
-    return eps
-
-
-def _find_kth_neighbor_slow(k, inds, i, x, y, z=None):
-    # Find the coordinates of the k'th neighbor in the joint distribution
-    # Do this by first searching to the left in x direction and then to
-    # the right in x direction until the x values become too large to
-    # qualify for inclusion in k smallest distances.
-    #
-    # This is not the most efficient method, especially in three dimensions.
-    # TODO: Replace with a more performant algorithm
-    cur_x = x[inds[i]]
-    cur_y = y[inds[i]]
-    if not z is None: cur_z = z[inds[i]]
-
-    eps = float("inf")
-    z_dist = 0
-    distances = np.full(k, eps)
-    for j in range(i-1, -1, -1):
-        x_dist = abs(cur_x - x[inds[j]])
-        if x_dist >= eps:
-            break
-
-        y_dist = abs(cur_y - y[inds[j]])
-        if not z is None: z_dist = abs(cur_z - z[inds[j]])
-        cur_dist = max(x_dist, y_dist, z_dist) # L-infinity norm
-        if (cur_dist < eps):
-            # Replace the largest distance in the array, then re-sort
-            distances[k-1] = cur_dist
-            distances.sort()
-            eps = distances.max()
-    
-    for j in range(i+1, len(x)):
-        x_dist = abs(cur_x - x[inds[j]])
-        if x_dist >= eps:
-            break
-
-        y_dist = abs(cur_y - y[inds[j]])
-        if not z is None: z_dist = abs(cur_z - z[inds[j]])
-        cur_dist = max(x_dist, y_dist, z_dist)
-        if (cur_dist < eps):
-            distances[k-1] = cur_dist
             distances.sort()
             eps = distances.max()
     
@@ -276,36 +253,54 @@ def _check_parameters(k, x, y, cond=None):
         raise ValueError("k must be smaller than number of observations")
 
 
-class _BoxGrid2D:
+class _BoxGrid:
     """A helper for accessing the space in smaller blocks."""
 
-    def __init__(self, x : np.ndarray, y : np.ndarray, k : int):
-        # Each box contains k points on average
-        # (Benchmarked to be better than 2*k or 4*k)
-        split_size = int(math.sqrt(k * len(x)))
-        boxes_per_axis = math.ceil(len(x) / split_size)
+    def __init__(self, x : np.ndarray, y : np.ndarray, k : int, z : np.ndarray = None):
+        if z is None:
+            # Each box contains k points on average
+            # (Benchmarked to be better than 2*k or 4*k)
+            split_size = int(math.sqrt(k * len(x)))
+            xy_boxes = math.ceil(len(x) / split_size)
+            z_boxes = 1
+        else:
+            # The same rule but in three dimensions
+            split_size = int(math.pow(k * len(x) * len(x), 1/3))
+            xy_boxes = math.ceil(len(x) / split_size)
+            z_boxes = xy_boxes
 
         # Store the boxes in a dictionary keyed by block coordinates
-        self.boxes_per_axis = boxes_per_axis
+        self.xy_boxes = xy_boxes
+        self.z_boxes = z_boxes
         self.boxes = {}
-        for i in range(boxes_per_axis):
-            for j in range(boxes_per_axis):
-                self.boxes[(i,j)] = []
+        for i in range(xy_boxes):
+            for j in range(xy_boxes):
+                for k in range(z_boxes):
+                    self.boxes[(i,j,k)] = []
 
         # Now the real initialization: first find the split points
         self.x_splits = np.sort(x)[0:len(x):split_size]
         self.y_splits = np.sort(y)[0:len(x):split_size]
+        if not z is None:
+            self.z_splits = np.sort(z)[0:len(x):split_size]
+        else:
+            self.z_splits = np.full(1, -np.inf)
         
         # Then assign points to boxes based on those
         for i in range(len(x)):
-            box_x, box_y = self.find_box(x[i], y[i])
-            self.boxes[(box_x, box_y)].append((x[i], y[i]))
+            if z is None: zi = 0
+            else: zi = z[i]
 
-    def find_box(self, x : float, y : float):
+            box_x, box_y, box_z = self.find_box(x[i], y[i], zi)
+            self.boxes[(box_x, box_y, box_z)].append((x[i], y[i], zi))
+
+
+    def find_box(self, x : float, y : float, z : float):
         # The box index to use is the index of the largest split point
         # smaller than the coordinate. This is easy to get with bisect,
         # we just need to offset by one.
         # This algorithm is O(log n).
         box_x = bisect.bisect(self.x_splits, x) - 1
         box_y = bisect.bisect(self.y_splits, y) - 1
-        return (box_x, box_y)
+        box_z = bisect.bisect(self.z_splits, z) - 1
+        return (box_x, box_y, box_z)
