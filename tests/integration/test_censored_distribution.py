591657803e1cd653281aab220d14783ecaa2621e
Petri Laarne <petri.laarne@helsinki.fi>
2020-06-18 12:42:59 +0300

    Add integration tests of non-simple distributions

---
+++ tests/integration/test_censored_distribution.py
@@ -0,0 +1,70 @@
+"""A Gaussian distribution censored on one axis.
+
+There is no analytical expression for the MI, and the algorithm in ennemi
+expects a continuous distribution. Nevertheless, ennemi seems to work fine.
+"""
+
+from ennemi import estimate_mi
+from scipy.integrate import quad, dblquad # type: ignore
+from scipy.stats import norm, multivariate_normal as mvnorm # type: ignore
+import numpy as np # type: ignore
+import unittest
+
+class TestCensoredDistribution(unittest.TestCase):
+    def setUp(self) -> None:
+        # A normal distribution offset to the right, and censored at x < 0
+        self.cov = np.asarray([[1, 0.6], [0.6, 1]])
+        self.mean = np.asarray([1, 0])
+
+
+    def test_mi(self) -> None:
+        # Estimate the actual MI by numerical integration
+        expected = self.integrate_mi()
+
+        # Sample from the distribution
+        rng = np.random.default_rng(0)
+        sample = rng.multivariate_normal(self.mean, self.cov, 4000)
+        sample[:,0] = np.maximum(0, sample[:,0])
+
+        # Add low-amplitude noise to the data
+        sample += rng.normal(0.0, 1e-8, size=sample.shape)
+
+        # The estimated MI should be very close to the numerical one
+        actual = estimate_mi(sample[:,0], sample[:,1])
+
+        self.assertAlmostEqual(actual, expected, delta=0.015)
+
+
+    def integrate_mi(self) -> float:
+        # We divide the integral into three parts:
+        #  - x < 0, y arbitrary: identically zero
+        #  - x = 0, y arbitrary: a discontinuous probability mass
+        #  - x > 0, y arbitrary: the ordinary normal density
+        #
+        # It is more than sufficient to consider only (-4, 6) x (-6, 6)
+        # when computing the integrals.
+
+        # The density along x = 0
+        def point_func(y: float) -> float:
+            # The joint density is P(x <= 0, y)
+            joint_dens = quad(lambda x: mvnorm.pdf((x,y), self.mean, self.cov), -4, 0, epsrel=5e-3)[0]
+
+            # The marginal x density comes from the cumulative density
+            x_dens = norm.cdf(0, self.mean[0], self.cov[0,0])
+            y_dens = norm.pdf(y, self.mean[1], self.cov[1,1])
+
+            return joint_dens * np.log(joint_dens / (x_dens * y_dens))
+        
+        # The density on x > 0
+        def cont_func(y: float, x: float) -> float:
+            # Ordinary joint and marginal densities
+            joint_dens = mvnorm.pdf((x,y), self.mean, self.cov)
+            x_dens = norm.pdf(x, self.mean[0], self.cov[0,0])
+            y_dens = norm.pdf(y, self.mean[1], self.cov[1,1])
+
+            return joint_dens * np.log(joint_dens / (x_dens * y_dens))
+        
+        point_part = quad(point_func, -6, 6)
+        cont_part = dblquad(cont_func, 0, 6, -6, 6, epsrel=1e-3)
+
+        return point_part[0] + cont_part[0]
---
+++ tests/integration/test_mixture_distribution.py
@@ -0,0 +1,59 @@
+"""A mixture distribution that has no analytical expression for MI."""
+
+from ennemi import estimate_mi
+from scipy.integrate import dblquad # type: ignore
+from scipy.stats import norm, multivariate_normal as mvnorm # type: ignore
+import numpy as np # type: ignore
+import unittest
+
+class TestMixtureDistribution(unittest.TestCase):
+    def setUp(self) -> None:
+        # A mixture of two normal distributions
+        self.cov1 = np.asarray([[1, 0.6], [0.6, 1]])
+        self.cov2 = np.asarray([[1, -0.4], [-0.4, 1]])
+
+        self.mean1 = np.asarray([-1, -1])
+        self.mean2 = np.asarray([2, 0.5])
+
+
+    def test_mi(self) -> None:
+        # Estimate the actual MI by numerical integration
+        expected = self.integrate_mi()
+
+        # Create a random sample from the distribution
+        rng = np.random.default_rng(0)
+        full_sample = np.concatenate((
+            rng.multivariate_normal(self.mean1, self.cov1, 2000),
+            rng.multivariate_normal(self.mean2, self.cov2, 2000),
+        ))
+        small_sample = rng.choice(full_sample, 200, replace=False)
+
+        # Estimate the MI with two k values and sample sizes
+        small_k1 = estimate_mi(small_sample[:,0], small_sample[:,1], k=1)
+        small_k3 = estimate_mi(small_sample[:,0], small_sample[:,1], k=3)
+        full_k2 = estimate_mi(full_sample[:,0], full_sample[:,1], k=2)
+        full_k40 = estimate_mi(full_sample[:,0], full_sample[:,1], k=40)
+
+        # With low sample size, increasing k should increase accuracy
+        self.assertLess(abs(small_k3 - expected), abs(small_k1 - expected))
+        self.assertAlmostEqual(small_k3, expected, delta=0.05)
+
+        # With high sample size, decreasing k should increase accuracy
+        self.assertLess(abs(full_k2 - expected), abs(full_k40 - expected))
+        self.assertAlmostEqual(full_k2, expected, delta=0.01)
+
+
+    def integrate_mi(self) -> float:
+        def f(y: float, x: float) -> float:
+            joint_dens = 0.5 * (mvnorm.pdf((x,y), self.mean1, self.cov1)\
+                + mvnorm.pdf((x,y), self.mean2, self.cov2))
+            x_dens = 0.5 * (norm.pdf(x, self.mean1[0], self.cov1[0,0])\
+                + norm.pdf(x, self.mean2[0], self.cov2[0,0]))
+            y_dens = 0.5 * (norm.pdf(y, self.mean1[1], self.cov1[1,1])\
+                + norm.pdf(y, self.mean2[1], self.cov2[1,1]))
+
+            return joint_dens * np.log(joint_dens / (x_dens * y_dens))
+        
+        # This rectangle contains most of the probability mass,
+        # and we can safely reduce the accuracy too
+        return dblquad(f, -6, 6, -6, 6, epsrel=1e-3)[0]
