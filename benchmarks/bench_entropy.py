14699ab34fec0d0524d4e151ec2301d337c65021
Petri Laarne <petri.laarne@helsinki.fi>
2020-06-23 16:12:49 +0300

    Implement conditional entropy estimation

--- benchmarks/bench_entropy.py
+++ benchmarks/bench_entropy.py
@@ -19,10 +19,12 @@ data = rng.multivariate_normal([0, 0, 0, 0], cov, size=4000)
 bench_1d = "estimate_entropy(data[:N,0], k=3)"
 bench_4d = "estimate_entropy(data[:N,:], k=3, multidim=True)"
 bench_independent = "estimate_entropy(data[:N,:], k=3)"
+bench_cond = "estimate_entropy(data[:N,:3], k=3, cond=data[:N,3])"
 
 for (name, bench) in [ ("1D", bench_1d),
-                       ("4 x 1D", bench_independent),
-                       ("4D", bench_4d) ]:
+                       ("4x1D", bench_independent),
+                       ("4D", bench_4d),
+                       ("Cond 3x1D", bench_cond) ]:
     for n in [ 250, 1000, 4000 ]:
         res = timeit.repeat(bench, setup, repeat=5, number=1, globals={"N": n})
-        print(f"{name:<6}, N={n:<4}: min={np.min(res):<6.3} s, mean={np.mean(res):<6.3} s")
+        print(f"{name:>9}, N={n:<4}: min={np.min(res):<6.3} s, mean={np.mean(res):<6.3} s")
