f17f586ab3317e1a7a6992010220c5389f8022f3
Petri Laarne <petri.laarne@helsinki.fi>
2022-01-11 13:30:41 +0200

    Bump minimum pytest for Python 3.10 compatibility

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     # At least pandas requires numpy 1.17.3+ (security fixes), we should too
     install_requires = [ "numpy>=1.17.5", "numpy<2.0", "scipy~=1.4" ],
     extras_require = {
-        "dev": [ "pandas~=1.0", "pytest~=5.4", "mypy~=0.770" ]
+        "dev": [ "pandas~=1.0", "pytest~=6.2", "mypy~=0.770" ]
     }
 )
