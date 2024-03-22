4531e5a999fa959c138b7d5314acab82b6583a29
Petri Laarne <petri.laarne@helsinki.fi>
2024-03-22 09:50:22 +0200

    Bump up minimum dependencies and version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(description_file, encoding="utf-8") as f:
 
 setup(
     name = "ennemi",
-    version = "1.3.0",
+    version = "1.4.0",
     description = "Non-linear correlation detection with mutual information",
     long_description = long_description,
     long_description_content_type = "text/markdown",
@@ -25,10 +25,9 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Scientific/Engineering",
         "Topic :: Scientific/Engineering :: Information Analysis",
         "Topic :: Scientific/Engineering :: Mathematics",
@@ -46,9 +45,9 @@ setup(
 
     packages = [ "ennemi" ],
     package_data = { "ennemi": ["py.typed"] },
-    python_requires = "~=3.8",
-    install_requires = [ "numpy~=1.21", "scipy~=1.7" ],
+    python_requires = "~=3.10",
+    install_requires = [ "numpy>=1.23", "scipy~=1.9" ],
     extras_require = {
-        "dev": [ "pandas>1.0", "pytest~=6.2", "mypy~=0.991" ]
+        "dev": [ "pandas>1.0", "pytest~=8.0", "mypy~=1.9" ]
     }
 )
